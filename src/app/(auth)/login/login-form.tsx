8e14d3190cc8aa0eae811ae3088b5acbaf7f6070
pltnhan311 <92429264+pltnhan311@users.noreply.github.com>
2024-05-13 11:51:16 +0700

    update auth vid 1/2

--- src/app/(auth)/login/login-form.tsx
+++ src/app/(auth)/login/login-form.tsx
@@ -44,6 +44,19 @@ const LoginForm = () => {
       toast({
         description: result.payload.message
       })
+      const resultFromNextServer = await fetch('/api/auth', {
+        method: 'POST',
+        body: JSON.stringify(result),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(async (res) => {
+        const payload = await res.json()
+        const data = { status: res.status, payload }
+        if (!res.ok) throw data
+        return data
+      })
+      console.log(resultFromNextServer)
     } catch (error: any) {
       const errors = error.payload.errors as { field: string; message: string }[]
       const status = error.status as number
